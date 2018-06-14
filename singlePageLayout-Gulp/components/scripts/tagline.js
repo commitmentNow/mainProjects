6338afa7f69af6945d9de4660a9b2ec7522b7532
MarlenAw <marlenawsh3@gmail.com>
2018-06-14 11:35:11 +0300

    Processing task with gulp-coffee

---
+++ singlePageLayout-Gulp/components/scripts/tagline.js
@@ -0,0 +1,7 @@
+var fill;
+
+(fill = function(item) {
+  return $('.tagline').append(`${item}`);
+})('Trying out coffeeeeee');
+
+fill;
--- singlePageLayout-Gulp/gulpfile.js
+++ singlePageLayout-Gulp/gulpfile.js
@@ -1,6 +1,16 @@
 const gulp = require("gulp");
 const gutil = require("gulp-util");
+const coffee = require("gulp-coffee");
 
-gulp.task('log', () => {
-    gutil.log('Workflows are working just fine!'); //in CMD run: gulp log
-});
+// gulp.task('log', () => {
+//     gutil.log('Workflows are working just fine!'); //in CMD run: gulp log
+// });
+
+
+
+gulp.task('coffee', ()=> {
+    gulp.src('components/coffee/tagline.coffee')
+        .pipe(coffee({ bare: true })          //bare:true -> it compiles to JavaScript without putting it in a safety wrapper like it normally would.
+            .on('error', gutil.log))
+                .pipe(gulp.dest('components/scripts')) //gulp destination -> this would mean where we want to send the file to once this process is done.
+}); //in CMD type: gulp coffee
